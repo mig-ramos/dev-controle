3040d2df4ddac1fd27bad6cd1cff77de86089fe6
mig-ramos <mig.ramos10@gmail.com>
2024-12-13 00:10:46 -0300

    feat: create page dashboard and container

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -4,7 +4,7 @@ import { FiUser, FiLogOut } from 'react-icons/fi'
 export function Header() {
     return (
         <header className="w-full flex items-center px-2 py-4 bg-white h-20 shadow-sm">
-            <div className="w-full flex items-center justify-between max-w-7xl mx-auto">
+            <div className="w-full flex items-center justify-between max-w-6xl mx-auto">
                 <Link href={"/"}>
                     <h1 className="font-bold text-2xl pl-1 hover:tracking-widest duration-300">
                         <span className="text-blue-500">DEV</span> CONTROLE
