583db01f8217acbcb54e1008bef3cfc02c3e4364
mig-ramos <mig.ramos10@gmail.com>
2024-12-13 23:05:03 -0300

    feat: authenticated route

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,15 @@
-import { Container } from "@/components/container"
+import { Container } from "@/components/container";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function Dashboard() {
+export default async function Dashboard() {
+    const session = await getServerSession(authOptions);
+    // console.log(session)
+
+    if (!session || !session.user) {
+        redirect("/")
+    }
     return (
         <Container>
             <h1>Pagina Dashboard</h1>
