fa96198da7289702d7bc772612ffd17a27b12174
Miguel Arcanjo <mig.ramos10@gmail.com>
2024-12-20 20:43:31 -0300

    feat: list all tickets

--- src/app/dashboard/components/ticket/index.tsx
+++ src/app/dashboard/components/ticket/index.tsx
@@ -1,23 +1,34 @@
-import { FiTrash2, FiFile } from "react-icons/fi"
+import { CustomerProps } from "@/utils/customer.type";
+import { TicketProps } from "@/utils/ticket.type";
+import { FiTrash2, FiFile } from "react-icons/fi";
 
-export function TicketItem() {
-    return (
-        <>
-            <tr className="border-b-2 border-b-slate-200 h-16 last:border-b-0 bg-slate-100 hover:bg-gray-200 duration-300">
-                <td className="text-left pl-1">Mercado Silva</td>
-                <td className="text-left hidden sm:table-cell">01/12/2024</td>
-                <td className="text-left">
-                    <span className="bg-green-500 px-2 py-1 rounded">ABERTO</span>
-                </td>
-                <td className="text-left">
-                    <button className="mr-2">
-                        <FiTrash2 size={24} color="#ef4444" />
-                    </button>
-                    <button>
-                        <FiFile size={24} color="#3b82f6" />
-                    </button>
-                </td>
-            </tr>
-        </>
-    )
+interface TicketItemProps {
+  ticket: TicketProps;
+  customer: CustomerProps | null;
+}
+
+export function TicketItem({ customer, ticket }: TicketItemProps) {
+  return (
+    <>
+      <tr className="border-b-2 border-b-slate-200 h-16 last:border-b-0 bg-slate-100 hover:bg-gray-200 duration-300">
+        <td className="text-left pl-1">{customer?.name}</td>
+        <td className="text-left hidden sm:table-cell">
+          {ticket.created_at?.toLocaleDateString("pt-br")}
+        </td>
+        <td className="text-left">
+          <span className="bg-green-500 px-2 py-1 rounded">
+            {ticket.status}
+          </span>
+        </td>
+        <td className="text-left">
+          <button className="mr-2">
+            <FiTrash2 size={24} color="#ef4444" />
+          </button>
+          <button>
+            <FiFile size={24} color="#3b82f6" />
+          </button>
+        </td>
+      </tr>
+    </>
+  );
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,37 +5,59 @@ import { redirect } from "next/navigation";
 import Link from "next/link";
 import { TicketItem } from "./components/ticket";
 
+import prismaClient from "@/lib/prisma";
+
 export default async function Dashboard() {
-    const session = await getServerSession(authOptions);
+  const session = await getServerSession(authOptions);
 
-    if (!session || !session.user) {
-        redirect("/")
-    }
+  if (!session || !session.user) {
+    redirect("/");
+  }
 
-    return (
-        <Container>
-            <main className="mt-9 mb-2">
-                <div className="flex items-center justify-between">
-                    <h1 className="text-3xl font-bold">Chamados</h1>
-                    <Link href={"/dashboard/new"} className="bg-blue-500 px-4 py-1 rounded text-white">
-                        Abrir chamado
-                    </Link>
-                </div>
-                <table className="min-w-full my-2">
-                    <thead>
-                        <tr>
-                            <th className="font-medium text-left pl-1">CLIENTE</th>
-                            <th className="font-medium text-left hidden sm:block">CADASTRO</th>
-                            <th className="font-medium text-left">STATUS</th>
-                            <th className="font-medium text-left">#</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <TicketItem />
-                        <TicketItem />
-                    </tbody>
-                </table>
-            </main>
-        </Container>
-    )
+  const tickets = await prismaClient.ticket.findMany({
+    where: {
+      userId: session.user.id,
+      status: "ABERTO",
+    },
+    include: {
+      customer: true,
+    },
+  });
+
+  return (
+    <Container>
+      <main className="mt-9 mb-2">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold">Chamados</h1>
+          <Link
+            href={"/dashboard/new"}
+            className="bg-blue-500 px-4 py-1 rounded text-white"
+          >
+            Abrir chamado
+          </Link>
+        </div>
+        <table className="min-w-full my-2">
+          <thead>
+            <tr>
+              <th className="font-medium text-left pl-1">CLIENTE</th>
+              <th className="font-medium text-left hidden sm:block">
+                CADASTRO
+              </th>
+              <th className="font-medium text-left">STATUS</th>
+              <th className="font-medium text-left">#</th>
+            </tr>
+          </thead>
+          <tbody>
+            {tickets.map((ticket) => (
+              <TicketItem
+                key={ticket.id}
+                customer={ticket.customer}
+                ticket={ticket}
+              />
+            ))}
+          </tbody>
+        </table>
+      </main>
+    </Container>
+  );
 }
